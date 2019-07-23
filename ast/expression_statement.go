4715b1001e95e8d1712cb22bc24675a23b6fec4c
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-07-24 00:00:48 +0530

    add expression statement, implement Stringer interface

---
+++ ast/expression_statement.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2019 Ajitem Sahasrabuddhe <me@ajitem.com>
+// All rights reserved. Use of this source code is governed
+// by the MIT license that can be found in the LICENSE file.
+
+package ast
+
+import "go.ajitem.com/donut/token"
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (e *ExpressionStatement) statementNode() {}
+
+func (e *ExpressionStatement) TokenLiteral() string {
+	return e.Token.Literal
+}
+
+func (e *ExpressionStatement) String() string {
+	if e.Expression != nil {
+		return e.Expression.String()
+	}
+
+	return ""
+}
