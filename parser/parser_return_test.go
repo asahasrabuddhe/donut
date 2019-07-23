7de0b265a1cdceb1986552ea2f02d56eca980d15
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-07-23 23:48:00 +0530

    add return statement parsing

---
+++ parser/parser_return_test.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2019 Ajitem Sahasrabuddhe <me@ajitem.com>
+// All rights reserved. Use of this source code is governed
+// by the MIT license that can be found in the LICENSE file.
+
+package parser
+
+import (
+	"go.ajitem.com/donut/ast"
+	"go.ajitem.com/donut/lexer"
+	"testing"
+)
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral())
+		}
+	}
+}
