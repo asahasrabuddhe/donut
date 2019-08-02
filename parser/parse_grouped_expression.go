9419abfef0fb824fd028746061ba2df5ba3ea3c2
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-08-02 09:13:53 +0530

    add support for grouped expressions

---
+++ parser/parse_grouped_expression.go
@@ -0,0 +1,22 @@
+// Copyright (c) 2019 Ajitem Sahasrabuddhe <me@ajitem.com>
+// All rights reserved. Use of this source code is governed
+// by the MIT license that can be found in the LICENSE file.
+
+package parser
+
+import (
+	"go.ajitem.com/donut/ast"
+	"go.ajitem.com/donut/token"
+)
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
+}
