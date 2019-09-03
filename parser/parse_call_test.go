071aa7b3841172e979fc2c3ce035c81e6554b14d
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2019-09-03 22:43:49 +0530

    call expression parsing: wip

---
+++ parser/parse_call_test.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"go.ajitem.com/donut/ast"
+	"go.ajitem.com/donut/lexer"
+	"testing"
+)
+
+func TestCallExpressionParsing(t *testing.T) {
+	input := `add(1, 2 * 3, 4 + 5)`
+
+	l := lexer.NewLexer(input)
+
+	p := NewParser(l)
+
+	program := p.ParseProgram()
+
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statement. got=%d\n", len(program.Statements))
+	}
+
+	expressionStatement, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("expressionStatement is not ast.ExpressionStatement. got=%T\n", program.Statements[0])
+	}
+
+	callExpression, ok := expressionStatement.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("expressionStatement.Expression is not ast.CallExpression. got=%T\n", expressionStatement.Expression)
+	}
+
+	if !testIdentifier(t, callExpression.Function, "add") {
+		return
+	}
+
+	if len(callExpression.Arguments) != 3 {
+		t.Fatalf("call expression did not receive 3 arguments. got=%d\n", len(callExpression.Arguments))
+	}
+
+	testLiteralExpression(t, callExpression.Arguments[0], 1)
+	testInfixExpression(t, callExpression.Arguments[1], 2, "*", 3)
+	testInfixExpression(t, callExpression.Arguments[2], 4, "+", 5)
+}
