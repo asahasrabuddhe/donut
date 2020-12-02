7d5be9d0aba983fedbd93bdc1b53d7bfffb9b370
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-12-02 10:51:52 +0530

    Add support for parsing if expressions

---
+++ parser/parser_if.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2019 Ajitem Sahasrabuddhe <me@ajitem.com>
+// All rights reserved. Use of this source code is governed
+// by the MIT license that can be found in the LICENSE file.
+
+package parser
+
+import (
+	"go.ajitem.com/donut/ast"
+	"go.ajitem.com/donut/token"
+)
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.currentToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
